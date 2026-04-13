79b58072a289e50f306043e5c920bdc5f5d53122
astroJarHead <73755825+astroJarHead@users.noreply.github.com>
2026-04-13 11:47:27 -0700

    Clean up stock.py by removing comments and variables
    Removed comments and unused variables from stock.py.

---
+++ stock.py
@@ -0,0 +1,14 @@
+# Challenge question #1: create a stock class
+
+class Stock:
+    def __init__(self, ticker, price, company):
+        self.ticker = ticker
+        self.price = price
+        self.company = company
+
+    def get_description(self):
+        # description = self.ticker+": "+self.company+" -- $"+str(self.price)
+        # return description
+        # school solution: one line! 
+        return f"{self.ticker}: {self.company} -- ${self.price}"
+        
