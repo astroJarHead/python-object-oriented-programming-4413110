b021ded81cb225c4062a48e6641c53f1e21b709b
astroJarHead <73755825+astroJarHead@users.noreply.github.com>
2026-04-13 11:56:24 -0700

    Add comments to Stock class and methods

--- stock.py
+++ stock.py
@@ -2,11 +2,13 @@
 
 class Stock:
     # create the stock class: recall self references class itself
+    # with the __init__ method. These are instance attributes
     def __init__(self, ticker, price, company):
         self.ticker = ticker
         self.price = price
         self.company = company
-
+    # an instance method taking the class as first parameter
+    # that is how the moethod accesses the attributes in the class
     def get_description(self):
         # description = self.ticker+": "+self.company+" -- $"+str(self.price)
         # return description
