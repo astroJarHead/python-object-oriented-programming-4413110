550553ea178317cbdda02224b9cd667b25d78a8d
astroJarHead <73755825+astroJarHead@users.noreply.github.com>
2026-04-13 13:25:28 -0700

    Refactor classes to use inheritance for publications
    Changed per the lesson

--- Start/Ch 2/inheritance_start.py
+++ Start/Ch 2/inheritance_start.py
@@ -1,29 +1,34 @@
 # Python Object Oriented Programming by Joe Marini course example
 # Understanding class inheritance
 
-
-class Book:
-    def __init__(self, title, author, pages, price):
+class Publication:
+    def __init__(self, title, price):
         self.title = title
         self.price = price
+
+
+class Periodical(Publication):
+    def __init__(self, title, price, period, publisher):
+        super().__init__(title, price)
+        self.period = period
+        self.publisher = publisher
+
+
+class Book(Publication):
+    def __init__(self, title, author, pages, price):
+        super().__init__(title, price)
         self.author = author
         self.pages = pages
 
 
-class Magazine:
+class Magazine(Periodical):
     def __init__(self, title, publisher, price, period):
-        self.title = title
-        self.price = price
-        self.period = period
-        self.publisher = publisher
+        super().__init__(title, price, period, publisher)
 
 
-class Newspaper:
+class Newspaper(Periodical):
     def __init__(self, title, publisher, price, period):
-        self.title = title
-        self.price = price
-        self.period = period
-        self.publisher = publisher
+        super().__init__(title, price, period, publisher)
 
 
 b1 = Book("Brave New World", "Aldous Huxley", 311, 29.0)
@@ -33,3 +38,5 @@ m1 = Magazine("Scientific American", "Springer Nature", 5.99, "Monthly")
 print(b1.author)
 print(n1.publisher)
 print(b1.price, m1.price, n1.price)
+
+
